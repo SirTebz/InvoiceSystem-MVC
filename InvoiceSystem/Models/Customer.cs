c825ea69e98ae9109aa38cd5714debfc48c0bf9a
Teboho Mokgosi <62904711+SirTebz@users.noreply.github.com>
2025-08-04 23:31:43 +0200

    Install Packages and Add Models

---
+++ InvoiceSystem/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace InvoiceSystem.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        // For simplicity, password is stored plain text.
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string BillingAddress { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
---
+++ InvoiceSystem/Models/Template.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceSystem.Models
+{
+    public class Template
+    {
+        [Key]
+        public int id { get; set; }
+        [Required, MaxLength(200)]
+        public string TemplateName { get; set; }
+        [Required]
+        public string HtmlContent { get; set; }
+        public TemplateType TemplateType { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public string? UpdateBy { get; set; }
+    }
+}
